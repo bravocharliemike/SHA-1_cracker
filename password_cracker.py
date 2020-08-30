901d08f125c7e6756873c53d535edd934133d5d0
Bernie <u7050370@anu.edu.au>
2020-08-30 17:05:34 +1000

    Added hashing password functionality

--- password_cracker.py
+++ password_cracker.py
@@ -1,7 +1,14 @@
 import hashlib
 
 #def crack_sha1_hash(hash):
-    
-"""
-This is a test
-"""
+
+with open('top-10000-passwords.txt') as fin:
+    data = fin.readlines()
+    # Loop over the entire file creating single passwords for hashing
+    for line in data:
+        single_password = line.strip()
+        hashed_pwd = hashlib.sha1(single_password.encode())
+        hex_format = hashed_pwd.hexdigest()     # Hexadecimal format of SHA-1 hash
+
+        if hex_format == '9d4e1e23bd5b727046a9e3b4b7db57bd8d6ee684':
+            print(f'Found password and it is {hex_format}')
