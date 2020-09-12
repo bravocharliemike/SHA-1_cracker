4813fa5b596eb99e8eb392ce409ab8242108a76d
bravocharliemike <bernardo.camejo@hotmail.com>
2020-09-12 11:18:36 +1000

    Added test suite and refactored main function

--- password_cracker.py
+++ password_cracker.py
@@ -12,45 +12,48 @@ arguments = parser.parse_args()
 
 def crack_sha1_hash(hash, use_salts=False):
     """
-    The function takes in a SHA-1 hash of a password and returns the password if it is one of the passwords stored in the passfile used. 
+    The function takes in a SHA-1 hash of a password and returns the password 
+    if it is one of the passwords stored in the passfile used. 
     If the SHA-1 hash is NOT of a password in the database, return "Not found in database".
     
-    The function hashes each password from the passfile and compare it to the hash passed into the function.
+    The function hashes each password from the passfile and compare it to 
+    the hash passed into the function.
     
-    The function takes an optional second argument named use_salts. If set to true, each salt string from the file `known-salts.txt` 
-    is appended and prepended to each password from the passfile before hashing and before comparing it to the hash 
-    passed into the function.
+    The function takes an optional second argument named use_salts. 
+    If set to true, each salt string from the file `known-salts.txt` 
+    is appended and prepended to each password from the passfile before 
+    hashing and before comparing it to the hash passed into the function.
     """
 
     # Prepare the file for hashing the cleartext passwords
     with open(arguments.passfile) as fin:
-        passwords = fin.read()
+        passwords = fin.read().split()
+    # Prepare the file with salts
+    with open('known-salts.txt') as fin:
+        salts = fin.read().split()
 
     # Key is hex digest and value the clear text password
     hashed_passwords = dict()
 
     # Loop over the entire file creating single passwords for hashing
-    for password in passwords.split('\n'):
-        single_pwd = password
+    for password in passwords:
         if use_salts:
-            with open('known-salts.txt') as f:
-                salts = f.read().split()
-                for salt in salts:
-                    for password in passwords.split('\n'):
-                        single_pwd = salt + password + salt
-                        hashed_pwd = hashlib.sha1(single_pwd.encode())
-                        hex_pwd = hashed_pwd.hexdigest()
-                        hashed_passwords[hex_pwd] = password
+            for salt in salts:
+                single_pwd_1 = salt + password
+                single_pwd_2 = password + salt
+                hashed_pwd_1 = hashlib.sha1(single_pwd_1.encode()).hexdigest()
+                hashed_pwd_2 = hashlib.sha1(single_pwd_2.encode()).hexdigest()
+                hashed_passwords[hashed_pwd_1] = password
+                hashed_passwords[hashed_pwd_2] = password
 
-        hashed_pwd = hashlib.sha1(single_pwd.encode())
-        hex_pwd = hashed_pwd.hexdigest()
-        hashed_passwords[hex_pwd] = single_pwd
+        hashed_pwd = hashlib.sha1(password.encode()).hexdigest()
+        hashed_passwords[hashed_pwd] = password
 
     # Check if the hash has matching clear text password
     for digest, clear_pwd in hashed_passwords.items():
         if hash == digest:
             return f'[+] Cracked: {clear_pwd}'
-
+            break
     return '[-] Not found in database'   # The password was not cracked
 
 
---
+++ test_password_cracker.py
@@ -0,0 +1,44 @@
+from password import crack_sha1_hash
+
+
+def test_crack_sha1_hash_1():
+    expected = "[+] Cracked: sammy123"  # include here a clear text password
+    actual = crack_sha1_hash("b305921a3723cd5d70a375cd21a61e60aabb84ec")
+    assert actual == expected
+
+
+def test_crack_sha1_hash_2():
+    expected = "[+] Cracked: abacab" 
+    actual = crack_sha1_hash("c7ab388a5ebefbf4d550652f1eb4d833e5316e3e")
+    assert actual == expected
+
+
+def test_crack_sha1_hash_3():
+    expected = "[+] Cracked: password"
+    actual = crack_sha1_hash("5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8")
+    assert actual == expected
+
+
+def test_crack_sha1_hash_4():
+    expected = "[-] Not found in database"
+    actual = crack_sha1_hash("91e692cb8cb1da51963da68abbe7cf1b2044bd79")
+    assert actual == expected
+
+
+def test_crack_sha1_hash_salted_1():
+    expected = '[+] Cracked: superman'
+    actual = crack_sha1_hash("53d8b3dc9d39f0184144674e310185e41a87ffd5", use_salts=True)
+    assert actual == expected
+
+
+def test_crack_sha1_hash_salted_2():
+    expected = '[+] Cracked: q1w2e3r4t5'
+    actual = crack_sha1_hash("da5a4e8cf89539e66097acd2f8af128acae2f8ae", use_salts=True)
+    assert actual == expected
+
+def test_crack_sha1_hash_salted_3():
+    expected = '[+] Cracked: bubbles1'
+    actual = crack_sha1_hash("ea3f62d498e3b98557f9f9cd0d905028b3b019e1", use_salts=True)
+    assert actual == expected
+
+
